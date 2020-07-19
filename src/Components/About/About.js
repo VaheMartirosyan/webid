0597a62025b355dcc5c266a150254ddd86e41263
Arman030598 <57864749+Arman030598@users.noreply.github.com>
2020-07-19 17:25:07 +0400

    'aboutanimation'

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -35,11 +35,14 @@ export default class Introducing extends React.Component {
 
             }
         });
-        this.state = {
-            arr:['001','002','003','004','005','006','007','008','009',
-                '010','011','012','013','014','015','016','017','018','019','020']
-        }
+        this.pl = gsap.timeline({
+            paused: true,
+            scrollTrigger: {
+                trigger: this.trigger,
+                scrub: true,
 
+            }
+        });
     }
     componentDidMount() {
         this.tl.to(".box", {
@@ -53,14 +56,18 @@ export default class Introducing extends React.Component {
         this.dl.to('.down', {
             x:2000,
             duration: 2
-        });
-        this.il.to(".box", {
-            force3D: true,
-            duration: 1,
-            // xPercent: -100,
-            ease: "power1.inOut",
-            stagger: { amount: 1 }
-        }).to(".about_last", { ease: "power1.out", duration: 1, translateX:'0%' }, 1)
+        }).to('.down',{color:'#ea0026'},0).to('.down',{color:'#ea0026'},1);
+        this.pl.to('.down2', {
+            x:-2000,
+            duration: 2
+        }).to('.down2',{color:'#ea0026'},0).to('.down',{color:'#ea0026'},1);
+        // this.il.to(".box", {
+        //     force3D: true,
+        //     duration: 1,
+        //     // xPercent: -100,
+        //     ease: "power1.inOut",
+        //     stagger: { amount: 1 }
+        // }).to(".about_last", { ease: "power1.out", duration: 1, translateX:'0%' }, 1)
 
     }
     render() {
@@ -77,14 +84,20 @@ export default class Introducing extends React.Component {
                 </span>
                 <div className={'boxes'}>
                     <img src={mask} alt=""/>
-                    {/*{this.state.arr.map((e,i)=>{*/}
-                    {/*    return(*/}
-                    {/*        <div key={i} className={`box boxed${i}`} ></div>*/}
-                    {/*    )*/}
-                    {/*})}*/}
                 </div>
                 <div className={'about_last'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, repudiandae?
+                   <span className="down2" >
+                    We are a team of young and ambitious talents desire to grow with every new project.
+                    <br/>
+                    We create budget fitted projects on time to improve Your business growing and customer relationships.
+                    <br/>
+                    Our passion is to provide innovative, highly functional, visually appealing and feature-rich websites, mobile applications and custom software that drive your business growth.
+                    <br/>
+                    Every client, regardless of the size or scope of the project, can expect nothing but the deep-dive of project development till its 100% complete.
+                </span>
+                </div>
+                <div className={'boxes2'}>
+                    <img src={mask} alt=""/>
                 </div>
             </section>
 
